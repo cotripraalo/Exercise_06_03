995adcf813135aaca5411563b01557b48aed70e0
ALONDRA COTRINA <ALONDRA COTRINA@ALONDRACOTRINA>
2017-09-04 16:48:02 -0500

    first

---
+++ src/exercise_06_03/Exercise_06_03.java
@@ -0,0 +1,54 @@
+package exercise_06_03;
+
+/*
+(Palindrome integer) Write the methods with the following headers
+
+// Return the reversal of an integer, i.e., reverse(456) returns 654
+public static int reverse(int number)
+
+// Return true if number is a palindrome
+public static boolean isPalindrome(int number)
+
+Use the reverse method to implement isPalindrome. A number is a palindrome
+if its reversal is the same as itself. Write a test program that prompts the
+user to enter an integer and reports whether the integer is a palindrome.
+*/
+import java.util.Scanner;
+
+
+public class Exercise_06_03 {
+
+    /** Main Method
+     * @param args */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // Create a Scanner
+        
+        // Prompt the user to enter an integer
+        System.out.println("Enter an integer: ");
+        int number = input.nextInt();
+        
+        // Report whether the integer is a palindrome.
+        System.out.println(number + (isPalindrome(number) ? "is" : "is not") + "a palindrome.");
+        
+    }
+ 
+/** Method isPalindrome returns true if number is a palindrome
+     * @param number
+     * @return  */
+    public static boolean isPalindrome(int number){
+        return number == reverse(number);
+    } 
+    
+   /** Method reverse returns the reversal of an integer
+     * @param number
+     * @return  */
+public static int reverse(int number){
+    String reverse =""; // Holds reversed number
+    String n= number + ""; // Convert number to string
+    // Reverse string
+    for( int i=n.length() -1; i>=0;i--){
+        reverse += n.charAt(i);
+    }
+    return Integer.parseInt(reverse); // Return reversed integer
+}
+}
